6e051b2c0cc5780f337840ef741a8fd52e3dc489
deadpitixd <86068615+deadpitixd@users.noreply.github.com>
2026-01-27 11:07:57 +0100

    Add ModItems class for item registration

---
+++ src/main/java/com/piti/ptm/item/ModItems.java
@@ -0,0 +1,20 @@
+package com.piti.ptm.item;
+
+import com.piti.ptm.PitisTech;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, PitisTech.MOD_ID);
+
+    public static final RegistryObject<Item> GEIGERCOUNTER = ITEMS.register("geigercounter",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
