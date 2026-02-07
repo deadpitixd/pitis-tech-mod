5e8d251a2541d28c7787af16d08869b382a75a19
deadpitixd <piotr.iwanski2012@gmail.com>
2026-02-07 14:03:34 +0100

    Finally the pipe works

--- src/main/java/com/piti/ptm/block/entity/PipeBlockEntity.java
+++ src/main/java/com/piti/ptm/block/entity/PipeBlockEntity.java
@@ -1,14 +1,17 @@
 package com.piti.ptm.block.entity;
 
+import com.piti.ptm.block.entity.ModBlockEntities;
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.protocol.game.ClientGamePacketListener;
 import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 
 public class PipeBlockEntity extends BlockEntity {
+
     private String filterFluidID = "";
 
     public PipeBlockEntity(BlockPos pos, BlockState state) {
@@ -20,33 +23,60 @@ public class PipeBlockEntity extends BlockEntity {
     }
 
     public void setFilterFluidID(String id) {
+        if (id == null) id = "";
+        if (id.equals(this.filterFluidID)) return;
+
         this.filterFluidID = id;
         setChanged();
+
         if (level != null) {
-            level.sendBlockUpdated(worldPosition, getBlockState(), getBlockState(), 3);
-            level.updateNeighborsAt(worldPosition, getBlockState().getBlock());
+            BlockState state = getBlockState();
+
+            level.sendBlockUpdated(worldPosition, state, state, 3);
+
+            if (level.isClientSide) {
+                level.sendBlockUpdated(worldPosition, state, state, 3);
+            }
         }
     }
 
     @Override
     protected void saveAdditional(CompoundTag nbt) {
-        nbt.putString("FluidID", filterFluidID);
         super.saveAdditional(nbt);
+        nbt.putString("FluidID", filterFluidID);
     }
 
     @Override
     public void load(CompoundTag nbt) {
         super.load(nbt);
-        this.filterFluidID = nbt.getString("FluidID");
+        filterFluidID = nbt.getString("FluidID");
+
+        if (level != null && level.isClientSide) {
+            BlockState state = getBlockState();
+            level.sendBlockUpdated(worldPosition, state, state, 3);
+        }
     }
 
     @Override
     public CompoundTag getUpdateTag() {
-        return saveWithoutMetadata();
+        CompoundTag tag = super.getUpdateTag();
+        tag.putString("FluidID", filterFluidID);
+        return tag;
     }
 
     @Override
     public Packet<ClientGamePacketListener> getUpdatePacket() {
         return ClientboundBlockEntityDataPacket.create(this);
+    }
+
+    @Override
+    public void onDataPacket(net.minecraft.network.Connection net, ClientboundBlockEntityDataPacket pkt) {
+        super.onDataPacket(net, pkt);
+        handleUpdateTag(pkt.getTag());
+    }
+
+    @Override
+    public void handleUpdateTag(CompoundTag tag) {
+        load(tag);
     }
 }
